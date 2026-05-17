4098c1753a152ab8eac665873ef607be8736fe23
I m r a a n <imrn.dev@gmail.com>
2026-05-17 20:24:57 +0530

    Auth updated from static

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,21 @@
 import NextAuth from "next-auth";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { authOptions } from "@/lib/auth/options";
 
-export default NextAuth(authOptions);
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+    const forwardedHost = req.headers["x-forwarded-host"];
+    const host = Array.isArray(forwardedHost)
+        ? forwardedHost[0]
+        : forwardedHost || req.headers.host;
+
+    const forwardedProto = req.headers["x-forwarded-proto"];
+    const protocol = Array.isArray(forwardedProto)
+        ? forwardedProto[0]
+        : forwardedProto || (host?.includes("localhost") ? "http" : "https");
+
+    if (process.env.NODE_ENV === "production" && host) {
+        process.env.NEXTAUTH_URL = `${protocol}://${host}`;
+    }
+
+    return await NextAuth(req, res, authOptions);
+}
